7bf31573027b6f13f302afa48bba633dba386f5d
Sean Wei <me@sean.taipei>
2020-05-04 14:09:56 +0800

    Squashed commit of the following:
    commit 16fe5f43e4ceb1edd5c4cd544d3e3fc6d2577516
    Author: Sean Wei <me@sean.taipei>
    Date:   Mon May 4 13:57:56 2020 +0800
    
        Support update body in votes group

--- telegram-bot/message.php
+++ telegram-bot/message.php
@@ -3,24 +3,24 @@ require(__DIR__.'/../utils.php');
 require(__DIR__.'/../database.php');
 $db = new MyDB();
 
-if ($TG->ChatID < 0) {
-	if ($TG->ChatID == -1001268692253)
-		exit;  // xNTHU Votes group
+$text = $TG->data['message']['text'] ?? '';
 
-	$TG->sendMsg([
-		'text' => '目前尚未支援群組功能',
-		'reply_markup' => [
-			'inline_keyboard' => [
-				[
+if ($TG->ChatID < 0) {
+	if ($TG->ChatID != -1001268692253)
+		$TG->sendMsg([
+			'text' => '目前尚未支援群組功能',
+			'reply_markup' => [
+				'inline_keyboard' => [
 					[
 						'text' => '📢 靠清 2.0 頻道',
 						'url' => 'https://t.me/xNTHU'
 					]
 				]
 			]
-		]
-	]);
-	exit;
+		]);
+
+	if (substr($text, 0, 1) != '/')
+		exit;
 }
 
 $USER = $db->getUserByTg($TG->FromID);
@@ -46,8 +46,6 @@ if (!$USER) {
 	]);
 	exit;
 }
-
-$text = $TG->data['message']['text'] ?? '';
 
 if (substr($text, 0, 1) == '/') {
 	$text = substr($text, 1);
@@ -195,6 +193,38 @@ if (substr($text, 0, 1) == '/') {
 				$TG->sendMsg([
 					'text' => "Failed.\n\n" . json_encode($result, JSON_PRETTY_PRINT)
 				]);
+			break;
+
+		case 'update':
+			if ($TG->FromID != 109780439) {
+				$TG->sendMsg([
+					'text' => "此功能僅限管理員使用",
+				]);
+				exit;
+			}
+
+			[$column, $body] = explode(' ', $arg, 2);
+
+			if ($column != "body") {
+				$TG->sendMsg([
+					'text' => "Column '$column' unsupported."
+				]);
+				exit;
+			}
+
+			if (!preg_match('/^#投稿(\w{4})/um', $TG->data['message']['reply_to_message']['text'] ?? '', $matches)) {
+				$TG->sendMsg([
+					'text' => 'Please reply to submission message.'
+				]);
+				exit;
+			}
+			$uid = $matches[1];
+
+			$db->updatePostBody($uid, $body);
+
+			$TG->sendMsg([
+				'text' => "Done.\n"
+			]);
 			break;
 
 		case 'delete':
